4753d78f11144efdeea6dc192b3653b6ce45e7f0
Dmitriy Temesov <dt@temesoft.com>
2024-08-12 12:48:44 -0700

    * Updates

--- auth-box-web/src/main/java/com/authbox/web/config/security/SecurityConfiguration.java
+++ auth-box-web/src/main/java/com/authbox/web/config/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.authbox.web.config.MethodSecurityConfiguration;
 import com.authbox.web.config.Oauth2ServerProperties;
 import com.google.common.collect.ImmutableMap;
 import io.micrometer.core.annotation.Timed;
+import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,16 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -36,7 +40,7 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Import({Oauth2ServerProperties.class, MethodSecurityConfiguration.class})
 public class SecurityConfiguration {
 
-    private static final Map<String, String> ACCESS_PATH_PREFIX_AND_SCOPE_PREFIX = ImmutableMap.of(
+    private static final Map<String, String> ACCESS_PATH_PREFIX_AND_SCOPE_PREFIX = Map.of(
             "/organization", "organization",
             "/oauth2-user", "oauth2-user",
             "/oauth2-token", "oauth2-token",
@@ -48,7 +52,6 @@ public class SecurityConfiguration {
             "/secure/**"
     };
     private static final String[] ALLOWED = {
-            // standard
             "/",
             "/index.html",
             "/register.html",
@@ -79,31 +82,61 @@ public class SecurityConfiguration {
     private Oauth2ServerProperties oauth2ServerProperties;
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http
-                .authorizeRequests()
-                .requestMatchers(ALLOWED)
-                .permitAll();
-        http
-                .logout()
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(config -> config.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.authorizeHttpRequests(auth -> {
+                    auth.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll();
+                    auth.requestMatchers(ALLOWED).permitAll();
+
+                    ACCESS_PATH_PREFIX_AND_SCOPE_PREFIX.forEach((pathPatternPrefix, scopePrefix) -> {
+                        auth.requestMatchers(antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix),
+                                        antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix + "/**"))
+                                .access(webEx("hasAuthority('SCOPE_" + scopePrefix + "/read') || hasRole('USER') || hasRole('ADMIN')"));
+
+                        auth.requestMatchers(antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix + "/**/2fa-qr-code"))
+                                .access(webEx("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')"));
+
+                        auth.requestMatchers(antMatcher(HttpMethod.POST, API_PREFIX + pathPatternPrefix),
+                                        antMatcher(HttpMethod.POST, API_PREFIX + pathPatternPrefix + "/**"))
+                                .access(webEx("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')"));
+
+                        auth.requestMatchers(antMatcher(HttpMethod.PUT, API_PREFIX + pathPatternPrefix),
+                                        antMatcher(HttpMethod.PUT, API_PREFIX + pathPatternPrefix + "/**"))
+                                .access(webEx("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')"));
+
+                        auth.requestMatchers(antMatcher(HttpMethod.DELETE, API_PREFIX + pathPatternPrefix),
+                                        antMatcher(HttpMethod.DELETE, API_PREFIX + pathPatternPrefix + "/**"))
+                                .access(webEx("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')"));
+
+                    });
+
+                    auth.requestMatchers(Arrays.stream(SECURE)
+                            .map((Function<String, RequestMatcher>) AntPathRequestMatcher::antMatcher)
+                            .toArray(RequestMatcher[]::new)).authenticated();
+                    auth.anyRequest().authenticated();
+                }
+        );
+
+        http.logout(config -> config
                 .logoutSuccessUrl("/")
                 .logoutUrl("/logout")
-                .and()
-                .authorizeRequests(authorizeRequestsCustomizerSecure(false))
-                .formLogin()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true));
+
+        http.formLogin(config -> config
                 .loginPage("/login")
                 .failureUrl("/sign-in.html?error")
                 .defaultSuccessUrl("/secure/index.html")
-                .permitAll();
+                .permitAll());
 
-        http
-                .authorizeRequests(authorizeRequestsCustomizerSecure(true))
-                .oauth2ResourceServer()
-                .opaqueToken()
-                .introspectionUri(oauth2ServerProperties.getIntrospectionUri())
-                .introspectionClientCredentials(oauth2ServerProperties.getClientId(), oauth2ServerProperties.getClientSecret());
+        http.oauth2ResourceServer(config ->
+                config.opaqueToken(
+                        customizer -> customizer
+                                .introspectionUri(oauth2ServerProperties.getIntrospectionUri())
+                                .introspectionClientCredentials(oauth2ServerProperties.getClientId(), oauth2ServerProperties.getClientSecret())
+                )
+        );
         return http.build();
     }
 
@@ -124,36 +157,7 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    private Customizer<ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry> authorizeRequestsCustomizerSecure(final boolean isLastSetup) {
-        return registry -> {
-            ACCESS_PATH_PREFIX_AND_SCOPE_PREFIX.forEach((pathPatternPrefix, scopePrefix) -> {
-                registry.requestMatchers(antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix),
-                                antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix + "/**"))
-                        .access("hasAuthority('SCOPE_" + scopePrefix + "/read') || hasRole('USER') || hasRole('ADMIN')");
-
-                registry.requestMatchers(antMatcher(HttpMethod.GET, API_PREFIX + pathPatternPrefix + "/**/2fa-qr-code"))
-                        .access("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')");
-
-                registry.requestMatchers(antMatcher(HttpMethod.POST, API_PREFIX + pathPatternPrefix),
-                                antMatcher(HttpMethod.POST, API_PREFIX + pathPatternPrefix + "/**"))
-                        .access("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')");
-
-                registry.requestMatchers(antMatcher(HttpMethod.PUT, API_PREFIX + pathPatternPrefix),
-                                antMatcher(HttpMethod.PUT, API_PREFIX + pathPatternPrefix + "/**"))
-                        .access("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')");
-
-                registry.requestMatchers(antMatcher(HttpMethod.DELETE, API_PREFIX + pathPatternPrefix),
-                                antMatcher(HttpMethod.DELETE, API_PREFIX + pathPatternPrefix + "/**"))
-                        .access("hasAuthority('SCOPE_" + scopePrefix + "/write') || hasRole('ADMIN')");
-
-            });
-
-            registry.requestMatchers(Arrays.stream(SECURE)
-                    .map((Function<String, RequestMatcher>) AntPathRequestMatcher::antMatcher)
-                    .toArray(RequestMatcher[]::new)).authenticated();
-            if (isLastSetup) {
-                registry.anyRequest().authenticated();
-            }
-        };
+    private WebExpressionAuthorizationManager webEx(final String value) {
+        return new WebExpressionAuthorizationManager(value);
     }
 }
